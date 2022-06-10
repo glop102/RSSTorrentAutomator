8eda494fe40c53cd34ea60a41ed4fb6a1a2ec1f3
glop102 <alexlikeschess@gmail.com>
2022-06-10 00:38:39 -0400

    small cleanup

--- server.py
+++ server.py
@@ -1,46 +1,15 @@
 from fastapi import FastAPI
 import yaml
 
-from threads import downloads
-
 app = FastAPI(
     title="RSS Torrent Automater"
 )
-settings = {} #default to no settings
-try:
-    #TODO make the settings file parse location configurable
-    settings_file = open("settings.yaml","r")
-    settings = yaml.safe_load(settings_file)
-except FileNotFoundError:
-    print("Unable to open the settings file - Is this the first time running the program?")
-except Exception as e:
-    print(e)
 
 @app.on_event("startup")
 def startup():
     print("Starting the server...")
-    #Spawn Threads to have other sections of the program ready to run
-    #torrent thread
-    if "downloads" in settings:
-        downloads.start(settings["downloads"])
-    else:
-        downloads.start({})
-    pass
-    downloads.registerEndpoints(app)
 
 @app.on_event("shutdown")
 def shutdown():
     print("Stopping the server...")
     print("Signaling threads to stop...")
-    #Signal the threads to stop and then wait on them
-    #torrent thread
-    downloads.stop()
-
-    global settings
-    print("Writing settings to file")
-    settings["downloads"] = downloads.serializeSettings()
-    
-    #TODO make the settings file parse location configurable
-    settings_file = open("settings.yaml","w")
-    yaml.dump(settings,settings_file)
-    #print(yaml.dump(settings))
